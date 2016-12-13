321529e7c51a4eaad5968a9d010e50b3a7d49f30
Robert Melton <robert@robertmelton.com>
2016-12-13 19:30:57 +0000

    Writing a generator,lalala

--- generate_colors.go
+++ generate_colors.go
@@ -16,7 +16,7 @@ func main() {
 	diffAddRow := vimColorRow{FG: colors["Green1"], BG: colors["NONE"], Attr: "NONE"}
 	diffChangeRow := vimColorRow{FG: colors["Orange4"], BG: colors["NONE"], Attr: "NONE"}
 	diffDeleteRow := vimColorRow{FG: colors["Maroon"], BG: colors["NONE"], Attr: "NONE"}
-	diffTextRow := vimcolorRow{FG: colors["Navy"], BG: colors["NONE"], Attr: "NONE"}
+	diffTextRow := vimColorRow{FG: colors["Navy"], BG: colors["NONE"], Attr: "NONE"}
 	directoryRow := vimColorRow{FG: colors["DeepPink7"], BG: colors["NONE"], Attr: "NONE"}
 	errorRow := vimColorRow{FG: colors["Red1"], BG: colors["White"], Attr: "NONE"}
 	fadedRow := vimColorRow{FG: colors["SandyBrown"], BG: colors["NONE"], Attr: "NONE"}
@@ -24,33 +24,41 @@ func main() {
 	matchParenRow := vimColorRow{FG: colors["White"], BG: colors["Navy"], Attr: "NONE"}
 	noneRow := vimColorRow{FG: colors["NONE"], BG: colors["NONE"], Attr: "NONE"}
 	normalRow := vimColorRow{FG: colors["Black"], BG: colors["Cornsilk"], Attr: "NONE"}
-	searchRow := vimColorRow{FG: colors["Green1"], BG: colors["White"], Attr: "NONE"}
+	searchRow := vimColorRow{FG: colors["White"], BG: colors["Green1"], Attr: "NONE"}
 	spellRow := vimColorRow{FG: colors["Purple2"], BG: colors["NONE"], Attr: "underline"}
 	statusLineRow := vimColorRow{FG: colors["Black"], BG: colors["Plum2"], Attr: "NONE"}
 	vertSplitRow := vimColorRow{FG: colors["Black"], BG: colors["LightCyan2"], Attr: "NONE"}
-	visualSelectRow := vimColorRow{FG: colors["FG"], BG: colors["LightGoldenrod4"], Attr: "NONE"}
+	visualSelectRow := vimColorRow{FG: colors["FG"], BG: colors["LightGoldenrod5"], Attr: "NONE"}
 	heavyCommentRow := vimColorRow{FG: colors["DeepPink8"], BG: colors["NONE"], Attr: "NONE"}
-	heavyStringRow := vimColorRow{FG: colors["Grey
+	heavyStringRow := vimColorRow{FG: colors["Grey100"], BG: colors["NONE"], Attr: "NONE"}
+	heavyLineRow := vimColorRow{FG: colors["DeepPink8"], BG: colors["NONE"], Attr: "NONE"}
 
 	pmenuRow := reverse(normalRow)
 	visualNOSRow := reverse(directoryRow)
+	commentRow := reverse(visualSelectRow)
 
 	nft := nofrilsTheme{
-		Name: "No Frils Acme",
-		Slug: "nofrils-acme",
+		Name:            "No Frils Acme",
+		Slug:            "nofrils-acme",
+		FadedRow:        fadedRow,
+		NoneRow:         noneRow,
+		NormalRow:       normalRow,
+		CommentRow:      commentRow,
+		HeavyCommentRow: heavyCommentRow,
+		HeavyStringRow:  heavyStringRow,
+		HeavyLineRow:    heavyLineRow,
 
 		Rows: []vimColorRow{
 			withName("Normal", normalRow),
 			withName("ColorColumn", cursorLineRow),
 			withName("MatchParen", matchParenRow),
 			withName("PmenuSel", statusLineRow),
-			withName("StatusLine", statusLineRow),
 			withName("Visual", visualSelectRow),
-			withName("TabLineFill", vertsplitRow),
 			withName("VisualNOS", visualNOSRow),
 			withName("Todo", diffAddRow),
 
-			withName("Comment", fadedRow),
+			withName("Comment", commentRow),
+
 			withName("FoldColumn", fadedRow),
 			withName("LineNr", fadedRow),
 			withName("NonText", fadedRow),
@@ -59,8 +67,15 @@ func main() {
 			withName("SignColumn", foldedRow),
 			withName("SpecialKey", foldedRow),
 
-			withName("StatusLineNC", vertsplitRow),
-			withName("VertSplit", vertsplitRow),
+			withName("TabLineFill", vertSplitRow),
+			withName("TabLine", vertSplitRow),
+			withName("StatusLineNC", vertSplitRow),
+			withName("VertSplit", vertSplitRow),
+
+			withName("TabLineSel", statusLineRow),
+			withName("StatusLine", statusLineRow),
+			withName("SneakLabelTarget", statusLineRow),
+			withName("SneakScope", statusLineRow),
 
 			withName("CursorColumn", cursorLineRow),
 			withName("CursorLineNr", cursorLineRow),
@@ -84,7 +99,6 @@ func main() {
 			withName("PmenuSbar", pmenuRow),
 			withName("Pmenu", pmenuRow),
 			withName("PmenuThumb", pmenuRow),
-			withName("TabLineSel", pmenuRow),
 			withName("WildMenu", pmenuRow),
 
 			withName("DiffAdd", diffAddRow),
@@ -100,12 +114,8 @@ func main() {
 			withName("SneakLabelMask", vertSplitRow),
 			withName("SneakTarget", vertSplitRow),
 
-			withName("SneakLabelTarget", statusLineRow),
-			withName("SneakScope", statusLineRow),
-
 			withName("Menu", noneRow),
 			withName("Scrollbar", noneRow),
-			withName("TabLine", noneRow),
 			withName("Tooltip", noneRow),
 			withName("Boolean", noneRow),
 			withName("Character", noneRow),
@@ -145,7 +155,6 @@ func main() {
 			withName("Underlined", noneRow),
 		},
 	}
-	log.Println(nft)
 	err := tmpl.Execute(os.Stdout, nft)
 	if err != nil {
 		log.Fatal(err)
@@ -178,9 +187,16 @@ type vimColorRow struct {
 }
 
 type nofrilsTheme struct {
-	Name string
-	Slug string
-	Rows []vimColorRow
+	Name            string
+	Slug            string
+	FadedRow        vimColorRow
+	NoneRow         vimColorRow
+	NormalRow       vimColorRow
+	CommentRow      vimColorRow
+	HeavyCommentRow vimColorRow
+	HeavyStringRow  vimColorRow
+	HeavyLineRow    vimColorRow
+	Rows            []vimColorRow
 }
 
 func Colors() map[string]color {
